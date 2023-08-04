9c77a81d282d6fa91aad6046b4c700678752a4a6
websfx <websfx@fooher.com>
2023-08-04 17:50:38 +0800

    Nginx 1.25.1

--- nginx/runContainer.sh
+++ nginx/runContainer.sh
@@ -4,11 +4,13 @@
 NGINX_VERSION="1.25.1"
 ALPINE_VERSION="3.18"
 
-docker stop nginx > /dev/null
-docker rm nginx > /dev/null
+#  docker stop nginx > /dev/null
+#  docker rm nginx > /dev/null
 
-docker pull websfx/nginx:${NGINX_VERSION}-alpine${ALPINE_VERSION}
-docker run -d -p 80:80 -p 443:443 --name nginx websfx/nginx:${NGINX_VERSION}-alpine${ALPINE_VERSION}
+#  docker pull websfx/nginx:1.25.1-alpine-3.18
+#  docker run -d -p 80:80 -p 443:443 --name nginx websfx/nginx:1.25.1-alpine-3.18
+docker pull websfx/nginx:${NGINX_VERSION}-alpine-${ALPINE_VERSION}
+docker run -d -p 80:80 -p 443:443 --name nginx websfx/nginx:${NGINX_VERSION}-alpine-${ALPINE_VERSION}
 
 #docker logs -f nginx
 docker ps -a |grep nginx
